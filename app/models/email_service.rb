48d1d8d0c25de7d8273c3157b2b405c3df763a37
Sterling Cobb <sterlingcobb@gmail.com>
2017-06-27 01:52:06 -0600

    fallback to secondary provider
    If the first email service fails for any reason, start using the second. No need to restart the server. ENV vars can also be updated since providers are cached

--- app/models/email_service.rb
+++ app/models/email_service.rb
@@ -4,13 +4,22 @@ require 'mail_gun_service'
 class EmailService
   def self.send_email(email)
     # cache instences
-    @mailgun ||= MailGunService.new
-    @sendgrid ||= SendGridService.new
+    @@mailgun ||= MailGunService.new
+    @@sendgrid ||= SendGridService.new
 
     if ENV['EMAIL_SERVICE'] == 'mailgun'
-      @mailgun.send_email email
+      @@dominant_provider = @@mailgun
+      @@backup_provider = @@sendgrid
     else
-      @sendgrid.send_email email
+      @@dominant_provider = @@sendgrid
+      @@backup_provider = @@mailgun
+    end
+
+    # try first provider, if it fails, attempted the second provider
+    begin
+      @@dominant_provider.send_email email
+    rescue
+      @@backup_provider.send_email email
     end
   end
 end
