db96d21b13d34c635ebc4931f44b2ab2d0c724ef
Sterling Cobb <sterlingcobb@gmail.com>
2017-06-26 19:46:42 -0600

    require params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  def create
+    email = email_params
+
+  end
+
+  private
+
+  def email_params
+    params.tap do |email_params|
+      email_params.require(:to)
+      email_params.require(:to_name)
+      email_params.require(:from)
+      email_params.require(:from_name)
+      email_params.require(:subject)
+      email_params.require(:body)
+    end
+  end
 end
--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,4 +1,19 @@
 class EmailController < ApplicationController
   def create
+    email = email_params
+    head :success
+  end
+
+  private
+
+  def email_params
+    params.tap do |email_params|
+      email_params.require(:to)
+      email_params.require(:to_name)
+      email_params.require(:from)
+      email_params.require(:from_name)
+      email_params.require(:subject)
+      email_params.require(:body)
+    end
   end
 end
